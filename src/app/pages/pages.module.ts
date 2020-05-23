e0ed9c80488b33b5f1aea49c63afeb390841d313
Manuel Sernaque <majesr1195@gmail.com>
2020-05-23 12:48:48 -0500

    Input , output y viewchild

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,24 +5,36 @@ import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
 
+// ng2-charts
+import { ChartsModule } from 'ng2-charts';
+
 // Modulos
 import { SharedModule } from '../shared/shared.module';
+
+import { FormsModule } from '@angular/forms';
 
 //Rutas
 import { PAGES_ROUTES } from './pages.routes';
 
+//temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
 @NgModule({
   declarations: [
     PagesComponent,
     DashboardComponent,
     ProgressComponent,
-    Graficas1Component
+    Graficas1Component,
+    IncrementadorComponent,
+    GraficoDonaComponent
 
   ],
   imports: [
     SharedModule,
-    PAGES_ROUTES
+    PAGES_ROUTES,
+    FormsModule,
+    ChartsModule
   ],
   exports: [
     DashboardComponent,
